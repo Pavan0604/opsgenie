6a32afcf31657aae827292f4610009716a11fa54
Pavan kumar N <56300560+Pavan0604@users.noreply.github.com>
2023-03-15 13:24:29 +0530

    Create teams.tf

---
+++ teams.tf
@@ -0,0 +1,14 @@
+resource "opsgenie_team" "splunk_escalation" {
+  name        = "Splunk_escalation"
+  description = "This team deals with all the splunk things"
+
+  member {
+    id   = "${opsgenie_user.neeraj.id}"
+    role = "admin"
+  }
+
+  member {
+    id   = "${opsgenie_user.swapnil.id}"
+    role = "user"
+  }
+}
