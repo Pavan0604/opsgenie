a9cb0972811c35a75c27d4e5e7a9674e7853aaf7
Pavan kumar N <56300560+Pavan0604@users.noreply.github.com>
2023-03-15 13:52:23 +0530

    Update teams.tf

--- teams.tf
+++ teams.tf
@@ -13,17 +13,3 @@ resource "opsgenie_team" "splunk_escalation" {
   }
 }
 
-resource "opsgenie_escalation" "splunk_team_escalation" {
-  name = "splunk_team_escalation"
-
-  rules {
-    condition   = "if-not-acked"
-    notify_type = "default"
-    delay       = 1
-
-    recipient {
-      type = "team"
-      id   = "${opsgenie_team.splunk_escalation.id}"
-      }
-    }
-}
