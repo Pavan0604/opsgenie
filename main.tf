a3ec1b190455d4514f12b8c78ebf0fc61132b6f4
Pavan kumar N <56300560+Pavan0604@users.noreply.github.com>
2023-03-15 12:27:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,24 @@ provider "opsgenie" {
   api_key = "key"
   api_url = "https://cloud-student.app.opsgenie.com/" #default is api.opsgenie.com
 }
+
+resource "opsgenie_user" "swapnil" {
+  username  = "swapnilkanere@gmail.com"
+  full_name = "Swapnil"
+  role      = "User"
+  locale    = "en_US"
+  timezone  = "America/New_York"
+  tags = ["sre", "opsgenie"]
+  skype_username = "skypename"
+  user_address {
+      country = "India"
+      state = "Karnataka"
+      city = "Bangalore"
+      line = "line"
+      zipcode = "998877"
+  }
+  user_details = {
+    key1 = "val1,val2"
+    key2 = "val3,val4"
+  }
+}
