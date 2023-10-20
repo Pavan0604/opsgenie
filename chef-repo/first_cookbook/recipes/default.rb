3019f15cf3c5ae38d93b6464943678f096f17b00
Pavan kumar N <56300560+Pavan0604@users.noreply.github.com>
2023-10-20 14:57:43 +0530

    Update default.rb

--- chef-repo/first_cookbook/recipes/default.rb
+++ chef-repo/first_cookbook/recipes/default.rb
@@ -4,13 +4,6 @@
 #
 # Copyright:: 2023, The Authors, All Rights Reserved.
 #
-#
-
-file "/home/ec2-user/default.txt" do
-  content "Hello, this is my first cookbook recipe\n"
-  action :create
-end
-
 
 include_recipe 'first_cookbook::ssm'
 include_recipe 'first_cookbook::persistent_storage'
